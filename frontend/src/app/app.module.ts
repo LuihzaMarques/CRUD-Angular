7ea92b5bd691db8b1e28503702f226fca57a3f37
LuihzaMarques <marquesluihza@gmail.com>
2023-10-19 01:13:40 -0300

    escopo e login page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './escopo/header/header.component';
+import { FooterComponent } from './escopo/footer/footer.component';
+import { LoginComponent } from './pages/login/login.component';
+import { BoxFormComponent } from './escopo/box-form/box-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    BoxFormComponent,
   ],
   imports: [
     BrowserModule,
